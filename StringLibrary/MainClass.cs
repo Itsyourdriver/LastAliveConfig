7b550c3db0be6425e2464b5d6b308415540ad22d
Itsyourdriver <73563507+Itsyourdriver@users.noreply.github.com>
2023-08-20 12:33:44 -0700

    Update MainClass.cs

--- StringLibrary/MainClass.cs
+++ StringLibrary/MainClass.cs
@@ -6,16 +6,26 @@ namespace lastalive
 {
     public class plugin
     {
-
+        public static Plugin Singleton;
+        [PluginConfig]
+        public Config Config;
+        
         [PluginEntryPoint("LastAlive, "1.0.0", "", "zInitial")]
         void OnEnabled()
         {
+
+            if (!Config.IsEnabled)
+                return;
+
+              Singleton = this;
             PluginAPI.Events.EventManager.RegisterEvents(this);
         }
 
         [PluginEvent(ServerEventType.PlayerDeath)]
         void OnPlayerDeath(PlayerDeathEvent e)
         {
+            Config config = Plugin.Singleton.Config;
+            
             int alive_count = 0;
             Player target = null;
             foreach (var p in Player.GetPlayers())
@@ -27,7 +37,7 @@ namespace lastalive
                 }
             }
             if (alive_count == 1)
-                target.ReceiveHint("You are the last one alive", 10);
+                target.ReceiveHint(config.Text, config.Duration);
         }
     }
 }
