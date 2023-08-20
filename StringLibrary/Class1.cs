5e31f11bcbe020724db2b8dc63c5443b8581465f
zInitialzed <zInitialiiz3d@gmail.com>
2023-08-20 14:42:41 +0800

    init

---
+++ StringLibrary/Class1.cs
@@ -0,0 +1,33 @@
+﻿using PluginAPI.Events;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+namespace testnamespace
+{
+    public class test
+    {
+
+        [PluginEntryPoint("example plugin", "1.0.0", "", "zInitial")]
+        void OnEnabled()
+        {
+            PluginAPI.Events.EventManager.RegisterEvents(this);
+        }
+
+        [PluginEvent(ServerEventType.PlayerDeath)]
+        void OnPlayerDeath(PlayerDeathEvent e)
+        {
+            int alive_count = 0;
+            Player target = null;
+            foreach (var p in Player.GetPlayers())
+            {
+                if (p.IsHuman)
+                {
+                    alive_count++;
+                    target = p;
+                }
+            }
+            if (alive_count == 1)
+                target.ReceiveHint("You are the last one alive", 10);
+        }
+    }
+}
